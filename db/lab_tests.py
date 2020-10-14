e50e9fe92bfd167d0203e4114c4ef65f8f7a65d4
spartanPAGE <spartanpageagain@gmail.com>
2020-10-14 10:41:31 +0200

    add more lab tests (mostly for S. aureus

--- db/lab_tests.py
+++ db/lab_tests.py
@@ -3,13 +3,52 @@ from models.LabTest import LabTest
 lab_tests_db = {
     "S. aureus": [
         LabTest(
-            name="test na czynnik zlepny CF"
+            name="test na czynnik zlepny CF",
+            category="S. aureus",
         ),
         LabTest(
-            name="test na białko A"
+            name="test na białko A",
+            category="S. aureus",
         ),
         LabTest(
-            name="test na polisacharyd otoczkowy S. aureus"
+            name="test na polisacharyd otoczkowy S. aureus",
+            category="S. aureus",
+        ),
+        LabTest(
+            name="test na hialuronidazę",
+            category="enzymy",
+        ),
+        LabTest(
+            name="test na stafylokinazę",
+            category="enzymy",
+        ),
+        LabTest(
+            name="test na DN-azę",
+            category="enzymy",
+        ),
+        LabTest(
+            name="test na proteinazę",
+            category="enzymy",
+        ),
+        LabTest(
+            name="test na hemolizyny",
+            category="toksyny",
+        ),
+        LabTest(
+            name="test na leukocydynę",
+            category="toksyny",
+        ),
+        LabTest(
+            name="test na toksyny epidermolityczne: eksfoliatyny ETA, ETB",
+            category="toksyny",
+        ),
+        LabTest(
+            name="test na enterotoksyny A-E",
+            category="toksyny",
+        ),
+        LabTest(
+            name="test na TSST1",
+            category="toksyny",
         )
     ]
 }
--- models/LabTest.py
+++ models/LabTest.py
@@ -1,5 +1,6 @@
 class LabTest:
-    def __init__(self, name, traits=[], requirements=[]):
+    def __init__(self, name, category="", traits=[], requirements=[]):
         self.name = name
+        self.category = category
         self.traits = traits
         self.requirements = requirements
